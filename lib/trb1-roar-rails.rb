b4e4937d87956428eb60539d1689980d6e2d1297
Phil Venegas <phil@serviceseeking.com.au>
2017-01-31 10:01:30 +1100

    latest

--- lib/trb1-roar-rails.rb
+++ lib/trb1-roar-rails.rb
@@ -3,40 +3,43 @@ require "trb1-roar/representer"
 require "trb1-roar/decorator"
 require "trb1-roar/rails/railtie"
 
-module Roar
-  autoload("XML", "trb1-roar/xml")
-  autoload("JSON", "trb1-roar/json")
+module Trb1
+  module Roar
+    autoload("XML", "trb1-roar/xml")
+    autoload("JSON", "trb1-roar/json")
 
-  module JSON
-    autoload("HAL", "trb1-roar/rails/hal")
-    autoload("JSONAPI", "trb1-roar/rails/json_api")
+    module JSON
+      autoload("HAL", "trb1-roar/rails/hal")
+      autoload("JSONAPI", "trb1-roar/rails/json_api")
+    end
+
+    autoload("Hypermedia", "trb1-roar/hypermedia")
   end
-
-  autoload("Hypermedia", "trb1-roar/hypermedia")
 end
 
+module Trb1
+  module Roar
+    module Rails
+      require 'trb1-uber/version'
+      def self.rails_version
+        Trb1::Uber::Version.new(::ActionPack::VERSION::STRING)
+      end
 
-module Roar
-  module Rails
-    require 'trb1-uber/version'
-    def self.rails_version
-      Trb1::Uber::Version.new(::ActionPack::VERSION::STRING)
+      if rails_version.~ 3.0
+        require 'trb1-roar/rails/rails3_0_strategy'
+      elsif rails_version.~ 3.1
+        require 'trb1-roar/rails/rails3_1_strategy'
+      elsif rails_version.~ 3.2
+        require 'trb1-roar/rails/rails3_2_strategy'
+      elsif rails_version.~ 4.2
+        require 'trb1-roar/rails/rails4_2_strategy'
+      else
+        # fallback to 4.0 strategy
+        require 'trb1-roar/rails/rails4_0_strategy'
+      end
+
+      autoload("TestCase", "trb1-roar/rails/test_case")
     end
-
-    if rails_version.~ 3.0
-      require 'trb1-roar/rails/rails3_0_strategy'
-    elsif rails_version.~ 3.1
-      require 'trb1-roar/rails/rails3_1_strategy'
-    elsif rails_version.~ 3.2
-      require 'trb1-roar/rails/rails3_2_strategy'
-    elsif rails_version.~ 4.2
-      require 'trb1-roar/rails/rails4_2_strategy'
-    else
-      # fallback to 4.0 strategy
-      require 'trb1-roar/rails/rails4_0_strategy'
-    end
-
-    autoload("TestCase", "trb1-roar/rails/test_case")
   end
 end
 
--- lib/trb1-roar/rails/json_api.rb
+++ lib/trb1-roar/rails/json_api.rb
@@ -4,4 +4,3 @@ Trb1::Roar::JSON::JSONAPI.class_eval do
   def to_json_api(*args);   to_json(*args);   end
   def from_json_api(*args); from_json(*args); end
 end
-
